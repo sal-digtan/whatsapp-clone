689795854a87746ab6da8f1dc1808e9642457367
sal-digtan <salman.digtan@gmail.com>
2024-10-18 17:49:26 +0500

    added tabview in main stack index.tsx

--- src/app/(main)/index.tsx
+++ src/app/(main)/index.tsx
@@ -4,16 +4,22 @@ import { verticalScale } from 'react-native-size-matters';
 import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 
 const FirstRoute = () => (
-    <View style={{ flex: 1, backgroundColor: '#ff4081' }} />
+    <View style={{ flex: 1, backgroundColor: '#fff' }} />
 );
 
 const SecondRoute = () => (
-    <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
+    <View style={{ flex: 1, backgroundColor: '#fff' }} />
 );
+
+const ThirdRoute = () => (
+    <View style={{ flex: 1, backgroundColor: '#fff' }} />
+);
+
 
 const renderScene = SceneMap({
     first: FirstRoute,
     second: SecondRoute,
+    third: ThirdRoute,
 });
 
 const renderTabBar = props => (
@@ -29,8 +35,9 @@ const Main = () => {
 
     const [index, setIndex] = useState(0);
     const [routes] = useState([
-        { key: 'first', title: 'First' },
-        { key: 'second', title: 'Second' },
+        { key: 'first', title: 'CHATS' },
+        { key: 'second', title: 'STATUS' },
+        { key: 'third', title: 'CALLS' },
     ]);
 
     return (
